5aa9526aea482c1de2a2f5fad69f9a8f7a33e9b6
alexirri14 <n00428998@upn.pe>
2026-05-16 03:04:47 -0500

    logo redimensionado

--- app/src/components/PageHero.tsx
+++ app/src/components/PageHero.tsx
@@ -5,7 +5,7 @@ interface PageHeroProps {
   backgroundImage?: string;
 }
 
-export default function PageHero({ title, breadcrumb, backgroundImage = '/RancheroFruits/images/Logo-1.jpg' }: PageHeroProps) {
+export default function PageHero({ title, breadcrumb, backgroundImage = '/RancheroFruits/images/Logo-1.jpeg' }: PageHeroProps) {
   return (
     <section className="relative h-[50vh] min-h-[400px] flex items-center justify-center overflow-hidden">
       <img
